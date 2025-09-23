9f6828dc482698be165ccf0efdd6d6d814361ad7
siam <niaz.siam.gakk@gmail.com>
2025-09-23 16:59:52 +0600

    Automated Proto Service Class Generation using Discount Proto File for Discount.API Microservice

--- src/Services/Discount.Grpc/obj/Debug/net7.0/Discount.Grpc.AssemblyInfo.cs
+++ src/Services/Discount.Grpc/obj/Debug/net7.0/Discount.Grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c393ea0c195e0780bc8817a64e39230d05c0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f992704d0f1c7a991a73bba6b5d4ad21a1c27ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
