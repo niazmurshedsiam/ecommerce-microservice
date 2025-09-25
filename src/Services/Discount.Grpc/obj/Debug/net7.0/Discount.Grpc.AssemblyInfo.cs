6ddfdcdcec06fc9b94eae524e789f8cc82c6af57
siam <niaz.siam.gakk@gmail.com>
2025-09-25 11:45:11 +0600

    Implementing a Repository Pattern with Redis

--- src/Services/Discount.Grpc/obj/Debug/net7.0/Discount.Grpc.AssemblyInfo.cs
+++ src/Services/Discount.Grpc/obj/Debug/net7.0/Discount.Grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0f52c143d791196a306553065caa211f0c505f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057c4b04b5cc5db3748314af1479f45fd980c955")]
 [assembly: System.Reflection.AssemblyProductAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Discount.Grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
